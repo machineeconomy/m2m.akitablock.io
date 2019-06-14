c0b384aa5391495d05ba1472ee01b751b84f3756
huhn <sehe89@gmail.com>
2019-06-14 17:47:12 +0200

    add manual deploy and set config

--- vue.config.js
+++ vue.config.js
@@ -19,5 +19,9 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
-  }
+  },
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/m2m.akitablock.io/'
+    : '/'
 };

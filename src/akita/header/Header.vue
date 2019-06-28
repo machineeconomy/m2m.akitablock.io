791aa90e8da238e6ac78180dfe0872bd0b4c7772
Maxuta Pavel <MaxutaPavel@gmail.com>
2019-06-28 14:03:59 +0300

    min img

--- src/akita/header/Header.vue
+++ src/akita/header/Header.vue
@@ -29,7 +29,7 @@ export default {
 
 <style>
 header {
-  background: url("./../../../public/chainTest.png") no-repeat,
+  background: url("./../../../public/chain.png") no-repeat,
     linear-gradient(to top left, #0BC5FE 0%, #5f46b1 50%, #ff2ea0 100%);
   background-size: cover;
   position: relative;

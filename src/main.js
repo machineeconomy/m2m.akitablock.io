625ff17f7feaef41e76b938fc6bae5c79e187f84
huhn <sehe89@gmail.com>
2019-06-29 02:12:46 +0200

    disable service worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Argon from "./plugins/argon-kit";
-import './registerServiceWorker'
+//import './registerServiceWorker'
 import moment from 'moment'
 
 

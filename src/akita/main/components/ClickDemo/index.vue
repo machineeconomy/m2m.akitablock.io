a9f66fe30e0194a75d8e5a3f38cf457be90e3ada
AKITA <36562673+akitablock@users.noreply.github.com>
2019-07-02 08:44:10 +0300

    Update index.vue

--- src/akita/main/components/ClickDemo/index.vue
+++ src/akita/main/components/ClickDemo/index.vue
@@ -3,7 +3,7 @@
     <div class="contain">
       <p style="font-size:20px;">In this Machine Economy alpha version demonstrator every participant has its own IOTA (DevNet) based service application.
       <br>
-     Each transaction triggers an automated machine-to-machine (M2M) payment and the related production process:</p>
+     Each transaction triggers an automated machine-to-machine (M2M) payment and the related energy delivery and production process:</p>
       <ol>
         <li style="font-size:20px;">Buy a new pair of headphones or a laptop</li>
         <li style="font-size:20px;">The rest of the process is completely automated. Just sit back and watch.</li>

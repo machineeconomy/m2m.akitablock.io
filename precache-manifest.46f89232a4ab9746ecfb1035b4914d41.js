8efb4313a5fb2dac81792d936bb3e0871ec82dd4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-03 12:54:23 +0000

    Deploy machineeconomy/m2m.akitablock.io to github.com/machineeconomy/m2m.akitablock.io.git:gh-pages

---
+++ precache-manifest.46f89232a4ab9746ecfb1035b4914d41.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2442eba9079723e5a9",
+    "url": "/css/app.2656e199.css"
+  },
+  {
+    "revision": "4c2442eba9079723e5a9",
+    "url": "/js/app.11ff2bf4.js"
+  },
+  {
+    "revision": "fcb9873094c60ee71a5b",
+    "url": "/css/chunk-vendors.5c3e7589.css"
+  },
+  {
+    "revision": "fcb9873094c60ee71a5b",
+    "url": "/js/chunk-vendors.dac8ad83.js"
+  },
+  {
+    "revision": "0e1e590c1f35d4a183b5a6056d618582",
+    "url": "/img/GitHub.0e1e590c.png"
+  },
+  {
+    "revision": "c63e49d75f5882272e384190ee430ad0",
+    "url": "/img/business_woman.c63e49d7.png"
+  },
+  {
+    "revision": "53104df3ba1d8db87a7f69f4f56576cd",
+    "url": "/img/EnergySolar.53104df3.png"
+  },
+  {
+    "revision": "2ba7e7e0ac4431175c1e83090acfb197",
+    "url": "/img/Robot1.2ba7e7e0.png"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "22315789f67cc1a1e30e10a6fffbd9b3",
+    "url": "/img/FactoryItems.22315789.png"
+  },
+  {
+    "revision": "08df94bdf65551e7c84df8745cdb7f2a",
+    "url": "/img/Robot2.08df94bd.png"
+  },
+  {
+    "revision": "e4607064eea8da8535fbff209a30ee58",
+    "url": "/img/M2M_IOTA_payment_process.e4607064.png"
+  },
+  {
+    "revision": "7242ac670295da07a65ce246f3285cbd",
+    "url": "/img/IOTAEcosystem.7242ac67.png"
+  },
+  {
+    "revision": "cb3b70ca886a17326cb010e3c4af171f",
+    "url": "/img/tangle.cb3b70ca.svg"
+  },
+  {
+    "revision": "f1de806d6d72f475de8ad9c1f62893dd",
+    "url": "/img/IOTALogo.f1de806d.png"
+  },
+  {
+    "revision": "93f4573f27841c80829a34b88c62f1b3",
+    "url": "/img/EnergyWind.93f4573f.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2e5331cb8aba8f95305e1496067e5101",
+    "url": "/img/Medium.2e5331cb.png"
+  },
+  {
+    "revision": "a162ff0ab7bab3d4ecd6376e86995cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8560d56407946ea3f556ea1a8001af29",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "b1b6b3c0b5ae151c86c499788f89bf28",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "f694c9335685adf9ef08843d09d9f385",
+    "url": "/img/1600px-Iota_logo.svg.png"
+  },
+  {
+    "revision": "a46771b9028e67415c06ed54a850c678",
+    "url": "/img/akita_logo_web.png"
+  },
+  {
+    "revision": "d6f7a1eac888dbde72c7dde0a56ca589",
+    "url": "/img/akita_logo_white.png"
+  },
+  {
+    "revision": "22315789f67cc1a1e30e10a6fffbd9b3",
+    "url": "/img/FactoryItems.png"
+  },
+  {
+    "revision": "4303f31d6aacd43948cd3629fbacc297",
+    "url": "/img/FactoryRobot.png"
+  },
+  {
+    "revision": "5014e0434b63761efa09a8897044ae85",
+    "url": "/img/github_logo.png"
+  },
+  {
+    "revision": "b1b6b3c0b5ae151c86c499788f89bf28",
+    "url": "/img/Iota_logo.png"
+  },
+  {
+    "revision": "4203538821aabe69fce216bc67acace9",
+    "url": "/img/linkedin_logo.png"
+  },
+  {
+    "revision": "44c14f3511783af52b0d0c43832c8572",
+    "url": "/img/iota_logo_blue.png"
+  },
+  {
+    "revision": "88c288a6b4e0ac7af7d48dc7200ca198",
+    "url": "/img/logo.88c288a6.svg"
+  },
+  {
+    "revision": "f98ce921ba9f42e2cf5bd971c88fce91",
+    "url": "/img/machines_logo.png"
+  },
+  {
+    "revision": "9e23abcb2c6428b3f191854dcd56d920",
+    "url": "/img/Text"
+  },
+  {
+    "revision": "b43a452a2e01aa6011c0b1ccff0a069e",
+    "url": "/img/twitter_logo.png"
+  },
+  {
+    "revision": "c1fc8b55559c630f85c7174e3d5fca25",
+    "url": "/img/SupplyChain.png"
+  },
+  {
+    "revision": "7195111502e92f4f5a0db1426a54cda6",
+    "url": "/img/WindTurbine.png"
+  },
+  {
+    "revision": "bfe0c2de782bd4941d617657fb444faa",
+    "url": "/img/chain-min.png"
+  },
+  {
+    "revision": "6aa842497903f628643bd369e652741c",
+    "url": "/img/chainTest.png"
+  },
+  {
+    "revision": "f21f9275914ab344c0562b77c4943ed1",
+    "url": "/img/SolarCell.png"
+  },
+  {
+    "revision": "e844954c2bd84a20bd3622083df2c941",
+    "url": "/img/MachineChain.png"
+  },
+  {
+    "revision": "2cef5bfd42d59b1913f60f9063fb66dc",
+    "url": "/img/AKITA - Virtual Lab - M2M Supply Chain - landing page .png"
+  },
+  {
+    "revision": "25efdaa64d966275183b514e2a30548c",
+    "url": "/img/FactoryItemsTest.png"
+  },
+  {
+    "revision": "504430a50df21a82346d16e2dbfbdaba",
+    "url": "/img/07.png"
+  },
+  {
+    "revision": "5d08c71cf05e40a61dbf38c9813c8ffe",
+    "url": "/img/chainBlue.png"
+  },
+  {
+    "revision": "cd8701c6d11597e72578f805c8c26d79",
+    "url": "/img/Wind Turbine.G15.2k.png"
+  },
+  {
+    "revision": "195f05b760ac8e890018ca20eb7d5ad8",
+    "url": "/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg.png"
+  },
+  {
+    "revision": "ffde970ef733204dfd1b945b0d98476a",
+    "url": "/img/chain.png"
+  },
+  {
+    "revision": "3069b2a9a0afd1e24164a6d3a564cd5c",
+    "url": "/img/Quadcopter.L14.2k.png"
+  },
+  {
+    "revision": "264923a1694ba1d3b834eddc40997873",
+    "url": "/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg2.png"
+  },
+  {
+    "revision": "8dbb9fcb1d89f498b0d6760733848f5d",
+    "url": "/img/imgonline-com-ua-Mirror-g8pwhdaqAqAokRBg1.png"
+  },
+  {
+    "revision": "9726011658b5f57bda36f0ef700c00ce",
+    "url": "/img/Business Woman.G15.2k.png"
+  },
+  {
+    "revision": "313206e16f5ea29b72a927846fde5e5f",
+    "url": "/img/Generic Hybrid Car.G03.2k.png"
+  },
+  {
+    "revision": "bac4c24033ee454acc39cf75a8e633e4",
+    "url": "/img/imgonline-com-ua-Mirror-rzQHR4x70x8.png"
+  },
+  {
+    "revision": "1ba8a589a773069e84223658bcf4242a",
+    "url": "/img/Factory Robot.G07.2k (1).png"
+  },
+  {
+    "revision": "112d13e796ea50e4f09deb579fe3f846",
+    "url": "/img/Industial Robot.G15.2k.png"
+  },
+  {
+    "revision": "df59d19c3f8be9ebdddbdabcd822816f",
+    "url": "/img/Solar Cell.H02.2k.png"
+  }
+];
